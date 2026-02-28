55182052890470ce1c0337c3662539f7d5b8de85
Luca Pal <184681939+luca-pal@users.noreply.github.com>
2026-02-28 23:25:13 +0100

    Added OpenAPI JWT bearer configuration and enabled Authorize button

---
+++ src/main/java/com/lucap/scubakeep/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package com.lucap.scubakeep.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Configures Swagger/OpenAPI to support JWT Bearer authentication.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+
+        return new OpenAPI()
+                // Global security requirement: Swagger will include the auth option
+                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .components(new Components().addSecuritySchemes(
+                        schemeName,
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ));
+    }
+}
